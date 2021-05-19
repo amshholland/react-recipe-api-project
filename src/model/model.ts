3dd04c5876f9b2b5f61e6b13f46038a8bfb047fe
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-19 17:53:01 -0400

    models not working, but have been updated

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,19 +1,19 @@
-export interface Search {
-    keyword: string;
-}
-export interface Results {
-    q: string;
-    results: Search[];
+export interface SearchResponse {
+    hits: CompleteHits[];
 }
 
-export interface Recipe {
+export interface CompleteHits {
+    recipe: CompleteRecipe;
+}
+
+export interface CompleteRecipe {
     label: string;
     image: string;
     url: string;
     healthLabels?: string[];
     dietLabels?: string[];
-    ingredientLines: string[];
-    ingredients: string[];
+    ingredientLines?: string[];
+    ingredients?: string[];
     calories: string;
     time: string;
 }
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,20 +1,34 @@
-import { Recipe } from "./../model/model";
+import { CompleteHits, CompleteRecipe, Recipe, Results, SearchResponse } from "./../model/model";
 
 const apiKey: string = process.env.REACT_APP_RECIPE_API_KEY || "";
 const apiId: string = process.env.REACT_APP_RECIPE_API_APP_ID || "";
-const credentials = btoa(apiKey);
+const credentials = btoa( apiKey );
 
-export function fetchRecipes(keyword: string): Promise<Recipe> {
-  return fetch(
-    `https://api.edamam.com/search?q=${keyword}&app_id=${apiId}&app_key=${apiKey}`,
-    {
-      headers: {
-        Authorization: `Basic ${credentials}`,
-      },
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+export function fetchRecipes( keyword: string ): Promise<SearchResponse> {
+    return fetch(
+        `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
+        {
+            headers: {
+                Authorization: `Basic ${ credentials }`,
+            },
+        }
+    )
+        .then( ( res ) => res.json() );
 }
+
+export function fetchComplete( keyword: string ): Promise<CompleteRecipe[]> {
+    return fetch(
+        `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
+        {
+            headers: {
+                Authorization: `Basic ${ credentials }`,
+            },
+        }
+            .then( ( res ) => res.json() )
+            .then( ( data: SearchResponse ) => {
+                return data.recipe;
+            } );
+    )
+}
+
+
