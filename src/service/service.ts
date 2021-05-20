873ed8bcecc627b17b6e888d1526a6f7672873b7
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-20 11:20:03 -0400

    corrected line 4 service

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,7 +1,7 @@
 import { RecipesResponse, SearchResponse } from "./../model/model";
 
 const apiKey: string = process.env.REACT_APP_RECIPE_API_KEY || "";
-const apiId: string = process.env.REACT_APP_RECIPE_API_APP_ID || "";
+const apiId: string = process.env.REACT_APP_RECIPE_API_ID || "";
 export function fetchAll( query: string ): Promise<SearchResponse[]> {
     return fetch(
         `https://api.edamam.com/search?q=${ query }&app_id=${ apiId }&app_key=${ apiKey }`
