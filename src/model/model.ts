aab2f4b38be10ff26a8f596a8cbda69bb43f6f11
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-20 08:00:42 -0400

    Modified service.ts

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,12 +1,14 @@
-export interface SearchResponse {
-    hits: CompleteHits[];
+
+export interface RecipesResponse { // Broader interface containing an ar
+    q: string;
+    from: number;
+    to: number;
+    more: boolean;
+    count: number;
+    recipes: Recipe[];
 }
 
-export interface CompleteHits {
-    recipe: CompleteRecipe;
-}
-
-export interface CompleteRecipe {
+export interface Recipe {
     label: string;
     image: string;
     url: string;
@@ -18,16 +20,28 @@ export interface CompleteRecipe {
     time: string;
 }
 
-export interface Favorites {
-    label: string;
-    image: string;
-    url: string;
-    healthLabels?: string[];
-    dietLabels?: string[];
-    ingredientLines: string[];
-    ingredients: string[];
-    calories: string;
-    time: string;
-    favorited: boolean;
-}
+// export interface CompleteRecipe {
+//     label: string;
+//     image: string;
+//     url: string;
+//     healthLabels?: string[];
+//     dietLabels?: string[];
+//     ingredientLines?: string[];
+//     ingredients?: string[];
+//     calories: string;
+//     time: string;
+// }
+
+// export interface Favorites {
+//     label: string;
+//     image: string;
+//     url: string;
+//     healthLabels?: string[];
+//     dietLabels?: string[];
+//     ingredientLines: string[];
+//     ingredients: string[];
+//     calories: string;
+//     time: string;
+//     favorited: boolean;
+// }
 
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,34 +1,48 @@
-import { CompleteHits, CompleteRecipe, Recipe, Results, SearchResponse } from "./../model/model";
+import { Recipe, RecipesResponse } from "./../model/model";
 
 const apiKey: string = process.env.REACT_APP_RECIPE_API_KEY || "";
 const apiId: string = process.env.REACT_APP_RECIPE_API_APP_ID || "";
 const credentials = btoa( apiKey );
 
-export function fetchRecipes( keyword: string ): Promise<SearchResponse> {
+export function fetchAll( searchTerm: string ): Promise<Recipe[]> {
     return fetch(
-        `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
+        `https://api.edamam.com/search?q=${ searchTerm }&app_id=${ apiId }&app_key=${ apiKey }`,
         {
             headers: {
                 Authorization: `Basic ${ credentials }`,
             },
         }
-    )
-        .then( ( res ) => res.json() );
+    ).then( ( res ) => res.json() )
+        .then( ( data: RecipesResponse ) => {
+            return data.recipes;
+        } );
 }
 
-export function fetchComplete( keyword: string ): Promise<CompleteRecipe[]> {
-    return fetch(
-        `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
-        {
-            headers: {
-                Authorization: `Basic ${ credentials }`,
-            },
-        }
-            .then( ( res ) => res.json() )
-            .then( ( data: SearchResponse ) => {
-                return data.recipe;
-            } );
-    )
-}
+// export function fetchRecipes( keyword: string ): Promise<SearchResponse> {
+//     return fetch(
+//         `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
+//         {
+//             headers: {
+//                 Authorization: `Basic ${ credentials }`,
+//             },
+//         }
+//     )
+//         .then( ( res ) => res.json() );
+// }
+
+// export function fetchComplete( keyword: string ): Promise<CompleteRecipe[]> {
+//     return fetch(
+//         `https://api.edamam.com/search?q=${ keyword }&app_id=${ apiId }&app_key=${ apiKey }`,
+//         {
+//             headers: {
+//                 Authorization: `Basic ${ credentials }`,
+//             },
+//         }
+//             .then( ( res ) => res.json() )
+//             .then( ( data: SearchResponse ) => {
+//                 return data.recipe;
+//             } );
+//     )
+// }
 
 
