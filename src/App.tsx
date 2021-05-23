d646871844c8d85eeca27e81efeafa61b6b32519
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-23 19:55:19 -0400

    corrected errors on App.tsx from merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,49 @@
 import "./App.css";
-import { Link } from "react-router-dom";
 
-import { Header } from "./components/Header";
-import React from "react";
-import { SearchForm } from "./components/SearchForm";
-import FavoriteView from "./components/FavoriteView";
-import { FavoriteContextProvider } from "./Context/favorite-context";
-import FavoredView from "./components/FavoredRoute";
-import { Favorite } from "./model/model";
-
-import FavoriteRoute from "./components/FavoriteRoute";
 import {
+  NavLink,
+  Redirect,
+  Route,
   BrowserRouter as Router,
   Switch,
-  Route,
-  Redirect,
-  NavLink,
 } from "react-router-dom";
+
+import FavoredView from "./components/FavoredRoute";
+import { Favorite } from "./model/model";
+import { FavoriteContextProvider } from "./Context/favorite-context";
+import FavoriteRoute from "./components/FavoriteRoute";
+import FavoriteView from "./components/FavoriteView";
+import { Header } from "./components/Header";
+import { Link } from "react-router-dom";
+import React from "react";
+import { SearchForm } from "./components/SearchForm";
 
 function App() {
   return (
-    <div className="App">;
-<<<<<<< HEAD
-  <Router>
-    <FavoriteContextProvider>
-      <h1>API Group Project</h1>
-      <nav>
-        <Link to="/favorites">See Saved Recipes</Link>{ " " }
-      </nav>
-      <Switch>
-        <Route path="/" exact>
-          <SearchForm />
-        </Route>
-        <Route path="/favorites/:num" exact>
-          <FavoredView />
-        </Route>
-        <Route>
-          <FavoriteRoute />
-        </Route>
-      </Switch>
-    </FavoriteContextProvider>
-  </Router>
-=======
-      <div className="components">
-        <Header />
-        <SearchForm />
-      </div>
->>>>>>> recipe
-    </div >;
-
+    <div className="App">
+      <Header />
+      <Router>
+        <FavoriteContextProvider>
+          <h1>API Group Project</h1>
+          <nav>
+            <Link to="/favorites">See Saved Recipes</Link>{ " " }
+          </nav>
+          <Switch>
+            <Route path="/" exact>
+              <SearchForm />
+            </Route>
+            <Route path="/favorites/:num" exact>
+              <FavoredView />
+            </Route>
+            <Route>
+              <FavoriteRoute />
+            </Route>
+          </Switch>
+        </FavoriteContextProvider>
+      </Router>
+    </div>
+  );
+}
     //     <div className="App">
     //       <header>
     //         Logo
