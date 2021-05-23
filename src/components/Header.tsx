2f0e1cce74cb79b67aa2c266ba33a14750fac760
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-23 19:01:18 -0400

    css updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import './Header.css';
 
-// import logo from '../public/Logo';
-
 export function Header() {
     return (
         <a href="/" className="Header">
-            <img className="logo" src={ process.env.PUBLIC_URL + '/Logo.png' } alt="Esurient logo" />
+            <img className="logo" src={ process.env.PUBLIC_URL + '/Logo.jpg' } alt="Esurient logo" />
         </a>
     );
 
