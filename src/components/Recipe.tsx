4659a9d54c28a9f300202e45e68611f437e31e0d
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-24 06:50:16 -0400

    Modal & Text formatting

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,5 +1,9 @@
 import './Recipe.css';
 
+import { AppModal, VerticallyCenteredModal } from './Modal';
+import { Button, Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+
 import { SearchResponse } from "../model/model";
 
 interface Props {
@@ -12,31 +16,38 @@ function Recipe( { recipe }: Props ) {
 
     return (
         <div className="Recipe" >
-            <div className="label">
-                <h3>{ recipe.label } FROM { recipe.source }</h3>
-            </div>
-            <div className="recipeImage" style={ { backgroundImage: `url(${ recipe.image })` } }>
-                <div className="details">
-                    {/* <div className="imageDiv">
-                        <img src={ recipe.image } alt={ recipe.label } />
-                    </div> */}
+            <Modal { ...recipe } size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <h3>{ recipe.label } FROM { recipe.source }</h3>
+                    </Modal.Title>
+                </Modal.Header>
 
-                    <div className="ingredients">
-                        <h4> Ingredients</h4>
-                        <ul>
-                            { ingredients.map( ( item ) => <li>{ item }</li> ) }
-                        </ul>
-                    </div>
+                <Modal.Body>
+                    {/* <div className="recipeImage" style={ { backgroundImage: `url(${ recipe.image })` } }> */ }
+                    <div className="details">
+                        {/* <div className="imageDiv">
+                        <img src={ recipe.image } alt={ recipe.label } /></div> */}
 
-                    <div className="otherDetails">
-                        <p>Calories: { recipe.calories }</p>
-                        <p>Time to Prepare: { recipe.totalTime }</p>
-                        <p>Dish Type: { recipe.mealType }</p>
-                        <p><a href={ recipe.url }>Full Recipe</a></p>
-                    </div>
-                </div >
-            </div>
-        </div >
+                        <div className="ingredients">
+                            <h4> Ingredients</h4>
+                            <ul>
+                                { ingredients.map( ( item ) => <li>{ item }</li> ) }
+                            </ul>
+                        </div>
+
+                        <div className="otherDetails">
+                            <p>Calories: { recipe.calories }</p>
+                            <p>Time to Prepare: { recipe.totalTime }</p>
+                            <p>Dish Type: { recipe.mealType }</p>
+                            <p><a href={ recipe.url }>Full Recipe</a></p>
+                        </div>
+                    </div >
+                    {/* </div> */ }
+                </Modal.Body>
+
+            </Modal>
+        </div>
 
     );
 }
